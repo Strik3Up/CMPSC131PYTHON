fd4f20c3b72e691453308d85cc98e8fd140564f9
Strik3Up <70175372+Strik3Up@users.noreply.github.com>
2020-09-25 12:18:17 -0400

    Update hw4.py

--- hw4/hw4.py
+++ hw4/hw4.py
@@ -11,25 +11,24 @@ gl = ['A', 'A-', 'B+', 'B', 'B-', 'C+', 'C', 'D', 'F']
 gl2 = ['A', 'A-', 'B+', 'B', 'B-', 'C+', 'C', 'D', 'F']
 gl3 = ['A', 'A-', 'B+', 'B', 'B-', 'C+', 'C', 'D', 'F']
 """
-if letter in gl:
   if letter == "A":
-		gp = 4.0
-	elif letter == "A-":
-		gp = 3.67
-	elif letter == "B+":
-		gp = 3.33
-	elif letter == "B":
-		gp = 3.0
-	elif letter == "B-":
-		gp = 2.67
-	elif letter == "C+":
-		gp = 2.33
-	elif letter == "C":
-		gp = 2.0
-	elif letter == "D":
-		gp = 1.0
-	else:
-		gp = 0.0
+    gp = 4.0
+  elif letter == "A-":
+    gp = 3.67
+  elif letter == "B+":
+    gp = 3.33
+  elif letter == "B":
+    gp = 3.0
+  elif letter == "B-":
+    gp = 2.67
+  elif letter == "C+":
+    gp = 2.33
+  elif letter == "C":
+    gp = 2.0
+  elif letter == "D":
+    gp = 1.0
+  else:
+   gp = 0.0
   float gp  
   return gp  
 
