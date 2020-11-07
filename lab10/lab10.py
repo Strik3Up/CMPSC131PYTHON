3c6edc29aad1da5e6b35db9508ca4198957f48dd
Strik3Up <70175372+Strik3Up@users.noreply.github.com>
2020-11-06 19:47:50 -0500

    Update lab10.py

--- lab10/lab10.py
+++ lab10/lab10.py
@@ -47,14 +47,13 @@ def run():
   il = list()
   c = 0
   while c < 1: 
-    i = input("Enter a string: \n")
+    i = input("Enter a string: ")
     if i == "done":
       ils = remove_duplicate_sorted(il)
       ild = list_to_dictionary(il)
       #print(f"List: [{il}]\n")
       print(f"Sorted List: {ils}\n")
       print(f"Dict: {ild}\n")
-      c + 1
       break
     else:
       il.append(i)
